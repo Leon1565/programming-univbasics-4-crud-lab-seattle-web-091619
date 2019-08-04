307a54320b3389ec877252ed172a3026e3c5bfc7
Cha Yang <chachaiyang3@gmail.com>
2019-08-04 19:29:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  my_games = ["LOL", "Summoners War", "Hearthstone", "Overwatch"]
 end
 
 def add_element_to_end_of_array(array, element)
